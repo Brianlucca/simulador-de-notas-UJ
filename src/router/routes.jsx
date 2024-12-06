b13cd620349e67ec2706786ee5c85468cf827fc9
Brianlucca <brianlucca2002@gmail.com>
2024-12-06 13:42:49 -0300

    refactor: ajeitando importação

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
-import History from "../pages/history/History";
-import GradeForm from "../pages/grade-form/GradeForm";
+import History from "../components/history/History";
+import GradeForm from "../components/grade-form/GradeForm";
 
 const RenderRoutes = () => {
   return (
