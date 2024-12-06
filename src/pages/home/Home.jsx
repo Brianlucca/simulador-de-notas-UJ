3e48eb5192321d026768a827f1e17c2ef055b8e5
Brianlucca <brianlucca2002@gmail.com>
2024-12-06 13:38:44 -0300

    feat: retornando os componentes na tela home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import GradeForm from "../grade-form/GradeForm";
+import History from "../history/History";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const Home = () => {
+  return (
+    <main className="bg-gray-100 min-h-screen p-4 flex">
+      <Sidebar />
+      <div className="w-full max-w-4xl grid gap-8 mt-8 ml-16 sm:ml-64">
+        <GradeForm />
+        <History />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
