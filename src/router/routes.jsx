b560060214734c90698ea38f42179ab9977d609b
Brianlucca <brianlucca2002@gmail.com>
2024-12-09 11:45:04 -0300

    feat: adicionado componente de avisos dinamicos

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import History from "../components/history/History";
 import GradeForm from "../components/grade-form/GradeForm";
+import WarningModal from "../components/warnings/WarningModal";
 
 const RenderRoutes = () => {
   return (
     <BrowserRouter>
+      <WarningModal />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/historico" element={<History />} />
