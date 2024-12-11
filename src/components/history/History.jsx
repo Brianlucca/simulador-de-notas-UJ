5642d24e2e526e96a96f3ccf1bf041b87c7fd771
Brianlucca <brianlucca2002@gmail.com>
2024-12-11 11:52:43 -0300

    quebra linha

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -21,7 +21,7 @@ const History = () => {
       try {
         await navigator.share({
           title: `${item.titulo}`,
-          text: `Confira minha média da matéria ${item.titulo}\n\nMédia Final: ${item.media}\nResultado: ${item.status}\n`,
+          text: `Confira minha média da matéria ${item.titulo}\n\nMédia Final: ${item.media}\nResultado: ${item.status}\n\n`,
           url: window.location.href,
         });
         alert("Histórico compartilhado com sucesso!");
